92eb12d39328af38338b1d137ea6b7b028cc7356
Paranoid <ajuph9224@gmail.com>
2022-04-03 19:01:28 +0530

    errors done

--- 24_errors/errors.go
+++ 24_errors/errors.go
@@ -1,10 +1,54 @@
-package man
+package main
 
 import (
   "fmt"
   "errors"
 )
 
-func main() {
+func f1(arg int) (int, error) {
+  if arg == 24 {
+    return -1, errors.New("24 cannot be accepted")
+  }
+  return arg + 3, nil
+}
 
+type argErr struct {
+  arg int
+  prob string
+}
+
+func (e *argErr) Error() string {
+  return fmt.Sprintf("%d - %s", e.arg, e.prob)
+}
+
+func f2(arg int) (int, error) {
+  if arg == 42 {
+    return -1, &argErr{arg: arg, prob: "can't work with it"}
+  }
+  return arg + 3, nil
+}
+
+func main() {
+  // f1 call
+  for _, i := range []int{7, 42} {
+    if r, e := f1(i); e != nil {
+      fmt.Println("f1 failed:", e)
+    } else {
+      fmt.Println("f1 worked:", r)
+    }
+  }
+  // f2 call
+  for _, i := range []int{7, 42} {
+    if r, e := f2(i); e != nil {
+      fmt.Println("f2 failed:", e)
+    } else {
+      fmt.Println("f2 worked:", r)
+    }
+  }
+
+  _, e := f2(42)
+  if ae, ok := e.(*argErr); ok {
+    fmt.Println(ae.arg)
+    fmt.Println(ae.prob)
+  }
 }
