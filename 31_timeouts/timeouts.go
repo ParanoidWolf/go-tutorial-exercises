fbdf6456be0f1c6c795db3eefe4768ead2b9dc1b
Paranoid <ajuph9224@gmail.com>
2022-04-05 08:06:34 +0530

    done timeout

--- 31_timeouts/timeouts.go
+++ 31_timeouts/timeouts.go
@@ -6,16 +6,29 @@ import (
 )
 
 func main() {
-  c1 := make(ch1 string, 1)
+  c1 := make(chan string, 1)
   go func() {
     time.Sleep(2 * time.Second)
     c1 <- "result 1"
   } ()
 
   select {
-  case rslt := <- ch1
-    fmt.println(rslt)
-  case <- time.After(time.Seconds)
-  fmt.print(:timeout2)
+  case rslt := <- c1:
+    fmt.Println(rslt)
+  case <- time.After(1 * time.Second): // time.After returns current time through the channel after the delay
+    fmt.Println("timeout 1")
+  }
+
+  c2 := make(chan string, 1)
+  go func() {
+    time.Sleep(2 * time.Second)
+    c2 <- "result 2"
+  } ()
+
+  select {
+  case rslt := <- c2:
+    fmt.Println(rslt)
+  case <- time.After(3 * time.Second):
+    fmt.Println("timeout 2")
   }
 }
